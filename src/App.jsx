b0d8918183f4be0e3fa8e9876cebafc22f6f68ba
unknown <magneticafternoon@gmail.com>
2024-09-23 21:16:52 +0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Page from './components/page'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 function App() {
 
